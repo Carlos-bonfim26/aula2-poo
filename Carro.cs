7338b3a8f4a68a82fd26bbc458276292e49519ee
CARLOS OLIVEIRA BONFIM <carlosbonfim722@gmail.com>
2024-06-20 12:59:20 -0300

    version1.0

---
+++ Carro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaPoo2
+{
+    internal class Carro
+    {
+      public  string cambio, cor, fabricante, nome;
+      public  DateOnly ano;
+        private bool ligado = false;
+
+        public void status()
+        {
+            Console.WriteLine($"nome: {nome}");
+            Console.WriteLine($"cor: {cor}");
+            Console.WriteLine($"ano: {ano}");
+            Console.WriteLine($"fabricante: {fabricante}");
+            Console.WriteLine($"câmbio: {cambio}");
+            Console.WriteLine($"está ligado: {ligado}");
+        }
+        public bool Ligar()
+        {
+            return ligado = true;
+        }
+        public bool desligar()
+        {
+            return ligado = false;
+        }
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using AulaPoo2;
+
+namespace AulaPoo2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Carro carro01 = new Carro();
+
+            carro01.fabricante = "Chevrolet";
+            carro01.ano = new DateOnly(1976, 1, 1);
+            carro01.cor = "Preto";
+            carro01.nome = "Opala";
+            carro01.cambio = "Manual";
+
+            Carro carro02 = new Carro();
+            carro02.fabricante = "Chevrolet";
+            carro02.ano = new DateOnly(2006, 6, 9);
+            carro02.cor = "vermelho";
+            carro02.nome = "Relâmpago Marquinhos Catchau";
+            carro02.cambio = "Automático";
+
+            carro01.status();
+            Console.WriteLine();
+            carro02.Ligar();
+            carro02.status();
+          
+        }
+    }
+}
